2b3c91eb0b8c2a5c12f0c3c4fd82785a7a114677
Calvin Hutcheon <chutcheon@mica.edu>
2019-11-14 09:38:58 -0800

    node server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const server = app.listen(7000, () => {
+  console.log(`Express running → PORT ${server.address().port}`);
+});
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Xalgorithms'
+  });
+});
+
+app.set('view engine', 'pug');
+
+app.use(express.static(__dirname + '/public'));
