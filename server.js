053b90c644291898ad89eb2f0b5cda87dc6c2597
Calvin Hutcheon <chutcheon@mica.edu>
2019-11-14 18:47:01 -0800

    changing port

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 
-const server = app.listen(7000, () => {
-  console.log(`Express running → PORT ${server.address().port}`);
-});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
 
 app.get('/', (req, res) => {
   res.render('index', {
