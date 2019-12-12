41d40c404d62e0981fbf8eb95be38599d15b4971
Calvin Hutcheon <chutcheon@mica.edu>
2019-12-11 18:01:44 -0800

    is it secure

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-
+var secure = require('ssl-express-www');
 var app = express();
 
-
+app.use(secure);
 
 //favicon
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -54,6 +54,7 @@ app.get('/bitclaims', (req, res) => {
 });
 
 app.use(express.static(__dirname + '/public'));
+
 
 let port = process.env.PORT;
 if (port == null || port == "") {
